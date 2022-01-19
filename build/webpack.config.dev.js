d84a168d7d651fed59fd0c3ae36947f81fbb7b86
John Hawley <john@hawley.cc>
2022-01-19 15:27:34 -0330

    Remove redundant config arg for dev server

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -2,7 +2,6 @@ const { merge } = require('webpack-merge');
 
 module.exports = merge(require('./webpack.config.base'), {
   mode: 'development',
-  watch: true,
   devtool: 'source-map'
 
   // All webpack configuration for development environment will go here
